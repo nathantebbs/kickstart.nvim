5a87b286ce20b27a8fd2471d9026f7ed92635d15
nathantebbs <nathan.tebbs2004@gmail.com>
2025-06-05 08:24:29 -0700

    added gruber darker theme

--- init.lua
+++ init.lua
@@ -774,28 +774,27 @@ require('lazy').setup({
     },
   },
 
-  -- COLORSCHEME GRUVBOX:
+  -- COLORSCHEME GRUBER DARKER:
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'ellisonleao/gruvbox.nvim',
+    'blazkowolf/gruber-darker.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('gruvbox').setup {
-        contrast = 'hard',
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
+      require('gruber-darker').setup {
+        bold = false,
+        italic = {
+          comments = false,
         },
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.o.background = 'dark'
-      vim.cmd.colorscheme 'gruvbox'
+      vim.cmd.colorscheme 'gruber-darker'
     end,
   },
 
@@ -871,7 +870,7 @@ require('lazy').setup({
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
       use_icons = true,
-      theme = '',
+      theme = 'gruber-darker',
     },
   },
 
